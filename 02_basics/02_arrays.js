464892f59f52b914b6a6e785714e82e85425bb8e
virushp278 <95963827+virushp278@users.noreply.github.com>
2024-03-09 09:53:08 +0000

    new

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,25 @@
+const marvel_heroes = ["thor","ironman","spiderman"]
+const dc_heroes = ["supermman","flash","batman"]
+// marvel_heroes.push(dc_heroes)
+// console.log(marvel_heroes);
+
+// const allHeroes = marvel_heroes.concat(dc_heroes)
+// console.log(allHeroes);
+
+// const all_new_heroes = [...marvel_heroes,...dc_heroes]
+// console.log(all_new_heroes);
+
+const another_array = [1,2,3,[4,5,6],7,7[6,7,[4,5]]]
+const real_another_array= another_array.flat(Infinity)
+console.log(real_another_array);
+
+
+console.log(Array.isArray("harsh"))
+console.log(Array.from("GITHUB"));
+console.log(Array.from({name:"GITHUB"}));//interesting\
+
+let score1 =100
+let score2 =200
+let score3 =300
+
+console.log(Array.of(score1,score2,score3));
